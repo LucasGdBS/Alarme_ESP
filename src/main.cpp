46360cc82f1a95c307a288b71eb6541c5fb9ce70
Lucas-PC <lgbs@cesar.school>
2023-09-29 23:02:19 -0300

    ta funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,37 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-void enviarDados(int ran){
+
+void enviarDados(float ran){
 
   // Cria variavel para conex~]ao http
   HTTPClient http;
 
   // Define URL e BODY para fazer o post
-  String url = "https://alarmeiot.up.railway.app/postdistancia";
-  String body = "dado=" + String(ran);
-  Serial.printf("Numero sorteado: %d\n", ran);
+  String url = "https://alarmeiot.up.railway.app/postdistancia/?dado=" + String(ran);
+  String body = String(ran, 4);
+  Serial.printf("Numero sorteado: %f\n", ran);
 
   // Inicia a conexão com a URL da API e adiciona o header (essa ultima parte eu n entendi direito)
   http.begin(url);
   
-  http.addHeader("Content-Type", "application/json");
+  http.addHeader("accept", "application/json");
 
   // Faz o post e recebe a resposta HTTP
-  int httpCode = http.POST(body);
+  int httpCode = http.POST({});
 
   // Verifica se o post foi um sucesso
-  if (httpCode == 200){
-    Serial.println("Dados enviados");
-  }else{
-    Serial.println("Erro ao enviar os dados-Erro:" + String(httpCode));
+  if (httpCode > 0){
+    Serial.printf("[HTTP] POST... código de status: %d\n", httpCode);
+    if (httpCode == HTTP_CODE_OK) {
+      String resposta = http.getString();
+      Serial.println("Resposta da API: " + resposta);
+    }
+  } else {
+    Serial.printf("[HTTP] Falha na conexão...\n");
   }
 
+  http.end();
 }
 
 void setup(){
@@ -34,7 +40,7 @@ void setup(){
   randomSeed(millis());
 
   // Conecta à rede WiFi
-  WiFi.begin("CLARO_2GLUCAS", "08052002");
+  WiFi.begin("Nome_WIFI", "Senha_WIFI");
 
   // Aguarda a conexão ser estabelecida
   while (WiFi.status() != WL_CONNECTED) {
@@ -45,17 +51,15 @@ void setup(){
   // Verifica se a conexão foi estabelecida
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("Conectado à internet!");
-  } else {
-    Serial.println("Falha na conexão!");
   }
 
 }
 
 void loop(){
 
-  int ran = random(100);
+  float ran = random(100)*1.0;
 
   enviarDados(ran);
-  delay(10000);
+  delay(5000);
 
 }
